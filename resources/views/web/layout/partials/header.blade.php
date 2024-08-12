c35a9c7f8af57b1ea9559bffe2f54fdcea824a79
lukasz <lukasz@localhost>
2024-08-12 13:02:15 +0200

    link do prenumuerata.swsmedia.pl

--- resources/views/web/layout/partials/header.blade.php
+++ resources/views/web/layout/partials/header.blade.php
@@ -1,4 +1,9 @@
 <header class="container px-0 pb-5">
+    <div style="background-color: #ff0039; color: white; padding: 10px; text-align: center; font-size: 1.5em; font-weight: 700;">
+    	<a href="#" style="text-decoration: none; color: white;">
+    		Kup prenumeratę online na prenumerata.swsmedia.pl!
+    	</a>
+    </div>
 	<a href="{{ route('web.index') }}">
 		<img class="img-responsive img-fluid w-100" src="{{ asset('storage/images/header.jpg') }}" />
 	</a>
